a92f6924f178c3e8e0779cb0837de0f13a61848b
Doug Kintop <dkintop@hotmail.com>
2019-09-11 15:55:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,7 +24,15 @@ def assign_rooms(speakers)
 end
 
 
-
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end 
+  
+  assign_rooms(attendees).each do |assignment|
+    puts assignment 
+  end
+end 
 
   
   
